ca7b88efaf087028371f3bf7dc2b459f63e8535f
sylwiaZon <sylwia.zon@gmail.com>
2018-10-23 22:48:53 +0200

    Removed folder

--- zad4/src/programe.java
+++
@@ -1,6 +0,0 @@
-public class programe {
-    public static void main(String[] argv){
-        Square square = new Square(10);
-        square.draw();
-    }
-}
--- zad5/src/MailException.java
+++
@@ -1,5 +0,0 @@
-public class MailException extends Exception {
-    public MailException(String message){
-        super(message);
-    }
-}
